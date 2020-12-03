a7b88de580f1cf5545684834e90569ff21d0a573
Arasefe <ismail_yildirim69@yahoo.com>
2020-12-03 13:52:41 -0600

    2December2020

--- src/test/java/com/cybertek/tests/interview/commonquestions/FluentWait_Practice.java
+++ src/test/java/com/cybertek/tests/interview/commonquestions/FluentWait_Practice.java
@@ -2,14 +2,15 @@ package com.cybertek.tests.interview.commonquestions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Wait;
+import org.testng.Assert;
 
 import java.time.Duration;
-import java.util.NoSuchElementException;
 import java.util.function.Function;
 
 public  class FluentWait_Practice {
@@ -19,19 +20,26 @@ public  class FluentWait_Practice {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/dynamic_loading/1");
-        driver.findElement(By.cssSelector("[id='start'] button")).click();
-        Wait<WebDriver> wait=new FluentWait<WebDriver>(driver).withTimeout(Duration.ofSeconds(30)).pollingEvery(Duration.ofSeconds(1)).ignoring(NoSuchElementException.class);
+        WebElement start=driver.findElement(By.xpath("//div[@id='start']/button"));
+        start.click();
+        // Waiting 30 seconds for an element to be present on the page, checking
+        // for its presence once every 5 seconds.
+        Wait<WebDriver> wait = new FluentWait<WebDriver>(driver)
+                .withTimeout(Duration.ofSeconds(30))
+                .pollingEvery(Duration.ofSeconds(5))
+                .ignoring(NoSuchElementException.class);
 
-        WebElement noo=wait.until(new Function< WebDriver, WebElement>() {
-            public WebElement apply(WebDriver driver){
-                if(driver.findElement(By.cssSelector("[id='finish'] h4")).isDisplayed()){
-                    return driver.findElement(By.cssSelector("[id='finish'] h4"));
-                }else{
+        WebElement foo = wait.until(new Function<WebDriver, WebElement>() {
+            public WebElement apply(WebDriver driver) {
+                WebElement result=driver.findElement(By.xpath("//div[@id='finish']/h4"));
+                if(!result.isDisplayed()){
                     return null;
                 }
+                Assert.assertTrue(result.getText().equals("Hello World!"),"Assertion FAILED");
+                return driver.findElement(By.xpath("//div[@id='finish']/h4"));
             }
         });
-        driver.quit();
+
 
     }
    
